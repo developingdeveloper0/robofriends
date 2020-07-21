3c0d46c5b15178cfe7f174164f4fe94b3c67ef89
BradMoore <48004534+developingdeveloper0@users.noreply.github.com>
2020-07-21 14:08:09 -0400

    Robofriends Udemy course project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 3,
+	name: 'Clementine Bauch',
+	username: 'Samantha',
+	email: 'Nathan@yesenia.net'
+},
+{
+	id: 4,
+	name: 'Patricia Lebsack',
+	username: 'Karianne',
+	email: 'Julianna.OConner@kory.org'
+},
+{
+	id: 5,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 6,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 7,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 8,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 9,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 10,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+}
+]
